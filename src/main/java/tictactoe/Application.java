5b7c57245bf2629eab28b4f1327e96711bac68bc
Seth Jackson <sethjackson@gmail.com>
2025-05-31 21:17:00 -0400

    Rename the main class

--- src/main/java/tictactoe/Application.java
+++ src/main/java/tictactoe/Application.java
@@ -3,7 +3,7 @@ package tictactoe;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
-public class Program {
+public class Application {
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
